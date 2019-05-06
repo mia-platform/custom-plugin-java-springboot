93833e5eb2ac6e2d82f52a9af249b088caa7b0a8
antonio.lasalandra <antonio.lasalandra@mia-platform.eu>
2019-05-06 18:18:50 +0200

    Add base package to scan in application and update example pom

--- application/src/main/java/eu/miaplatform/customplugin/springboot/example/Application.java
+++ application/src/main/java/eu/miaplatform/customplugin/springboot/example/Application.java
@@ -3,7 +3,7 @@ package eu.miaplatform.customplugin.springboot.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "eu.miaplatform.customplugin.springboot")
 public class Application {
 
     //Default server port is 3000
