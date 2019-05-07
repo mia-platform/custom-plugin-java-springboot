a489e3696401038a583f8e458524eb8fe70391d1
antonio.lasalandra <antonio.lasalandra@mia-platform.eu>
2019-05-07 10:46:53 +0200

    Add test directory

---
+++ application/src/test/java/eu/miaplatform/customplugin/springboot/CPRequestTest.java
@@ -0,0 +1,49 @@
+package eu.miaplatform.customplugin.springboot;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class CPRequestTest {
+
+    private CPRequest cpRequest;
+    private Options options = new Options();
+    private String id = "1234";
+    private String type = "admin";
+    private boolean backoffice = true;
+    private String groupsString = "group1,group2";
+    private List<String> groups = Arrays.asList("group1","group2");
+
+    @Before
+    public void setUp() {
+        cpRequest = new CPRequest(getRequest(options), options);
+    }
+
+    @Test
+    public void checkPlatformHeaders() {
+        assertEquals(id, cpRequest.getUserId());
+        assertEquals(type, cpRequest.getClientType());
+        assertEquals(backoffice, cpRequest.isFromBackOffice());
+        for (int i = 0; i < groups.size(); ++i) {
+            assertEquals(groups.get(i), cpRequest.getGroups().get(i));
+        }
+
+    }
+
+    private HttpServletRequest getRequest(Options options) {
+
+        MockHttpServletRequest req = new MockHttpServletRequest();
+
+        req.addHeader(options.getUserIdHeaderKey(), id);
+        req.addHeader(options.getClientTypeHeaderKey(), type);
+        req.addHeader(options.getBackofficeHeaderKey(), backoffice);
+        req.addHeader(options.getGroupsHeaderKey(), groupsString);
+
+        return req;
+    }
+}
